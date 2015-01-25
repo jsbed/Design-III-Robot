e5e0dfb02534ae2253535c82fae6dcfe2c2992db
dms1lva <dario@live.ca>
2015-01-24 23:07:38 -0500

    clean server code for streaming

--- SocketTCP/server.py
+++ SocketTCP/server.py
@@ -1,28 +1,23 @@
 import socket
 import cv2
 import numpy
-import sys
-from time import sleep
 
 
 PORT = 10000
 IP = '192.168.2.10'
 BUFFER_SIZE = 10000
 
-# Create a TCP/IP socket
+
+# Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = (IP, PORT)
-
 print('starting up on %s port %s' % server_address)
 sock.bind(server_address)
-#sock.listen(1)#not need for udp
+
+
 while True:
-    # Find connections
-    #connection, client_address = sock.accept()
-    #print(client_address)
     try:
         data = sock.recv(BUFFER_SIZE)
-        print(sys.getsizeof(data))
         nparr = numpy.fromstring(data, numpy.uint8)
         img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         c = cv2.waitKey(1)
@@ -31,5 +26,4 @@ while True:
         cv2.imshow("RGB", img_np)
 
     except Exception as e:
-        connection.close()
-        #print(e)
+        print(e)
