698a8ff919d40b0ced5f5fae18ba0675546844cf
jsbed <bedardjs97@hotmail.com>
2015-02-05 00:15:00 -0500

    changed 'hint' keyword for 'event'

--- Robot/src/Utilities/Observable.py
+++ Robot/src/Utilities/Observable.py
@@ -3,27 +3,28 @@ class Observable:
     def __init__(self):
         self._observers = {}
 
-    def attach(self, hint, observer):
-        if hint not in self._observers:
-            self._observers[hint] = [observer, ]
+    def attach(self, event, observer):
+        if event not in self._observers:
+            self._observers[event] = [observer, ]
         else:
-            if observer not in self._observers[hint]:
-                self._observers[hint].append(observer)
+            if observer not in self._observers[event]:
+                self._observers[event].append(observer)
             else:
-                raise ValueError("Observer is already registered to hint: ",
-                                hint)
+                raise ValueError("Observer is already registered to event: ",
+                                event)
 
-    def detach(self, hint, observer):
-        if hint not in self._observers:
-            raise KeyError("No observers are registered for the hint: ", hint)
+    def detach(self, event, observer):
+        if event not in self._observers:
+            raise KeyError("No observers are registered for the event: ",
+                           event)
         else:
-            if observer not in self._observers[hint]:
-                raise ValueError("Observer is not registered for the hint: ",
-                                 hint)
+            if observer not in self._observers[event]:
+                raise ValueError("Observer is not registered for the event: ",
+                                 event)
             else:
-                self._observers[hint].remove(observer)
+                self._observers[event].remove(observer)
 
-    def notify(self, hint):
-        if hint in self._observers:
-            for observer in self._observers[hint]:
-                observer.update(hint)
+    def notify(self, event):
+        if event in self._observers:
+            for observer in self._observers[event]:
+                observer.update(event)
