abae5e9e7a3bbef17fd8cbd2e96cf37ea9dc387b
jsbed <bedardjs97@hotmail.com>
2015-02-06 19:12:25 -0500

    added config file + config loader

--- Robot/src/Atlas.py
+++ Robot/src/Atlas.py
@@ -1,8 +1,8 @@
 import requests
 
-
-server_address = "https://132.203.14.228/"  # TODO: Create config file for URL
+from Configuration.Config import Config
 
 
 def get_question():
-    return requests.get(server_address, verify=False).json()["question"]
+    response = requests.get(Config().get_atlas_url(), verify=False)
+    return response.json()["question"]
--- Robot/src/main.py
+++ Robot/src/main.py
@@ -1,7 +1,11 @@
 import os
 
+from Configuration.Config import Config
 from Filler import CountryRepositoryFiller
 
+
+# Initiate configuration
+Config().loadConfig()
 
 # Fill Country repository
 flags_file_path = os.path.join("resources", "flags.csv")
