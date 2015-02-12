daa795846be0b8a3cab7350ebb9fd7055fbbecd2
dmsilva <dario@live.ca>
2015-02-12 15:44:00 -0500

    parser, started regex, some name changes: python modules names should be lowercase, check out pep8 B-)

---
+++ Robot/src/question_analysis/factbook_parsing/update_country_dump.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import os
+import shutil
+
+country_data_dir = Path('./factbook/geos')
+for country_file in country_data_dir.iterdir():
+    if country_file.is_dir():
+        shutil.rmtree(country_file.as_posix())
+
+    elif country_file.suffix != '.html':
+        print(country_file.name)
+        os.remove(country_file.as_posix())
