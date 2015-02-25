7054b0ddd5a5074610de65cd0efa44479d82c443
Jean-Samuel <bedardjs97@hotmail.com>
2015-02-24 19:28:58 -0500

    fixed point adjustor test

--- tests/test_path_finding/test_PointAdjustor.py
+++ tests/test_path_finding/test_PointAdjustor.py
@@ -1,5 +1,4 @@
 from collections.__main__ import Point
-from defer import return_value
 from unittest.mock import patch, MagicMock, Mock
 import unittest
 
@@ -10,7 +9,6 @@ from Robot.locators.localization import Localization
 from Robot.path_finding.point_adjustor import PointAdjustor
 
 
-@patch('Robot.path_finding.point_adjustor.config.Config')
 class GameTest(unittest.TestCase):
 
     @classmethod
@@ -23,70 +21,63 @@ class GameTest(unittest.TestCase):
         self._point_test = Point(0, 0)
 
     def _setup_config_mock(self, mock):
-
         a_mock = MagicMock()
-        a_mock.get_table_height = Mock(return_value=251)
-        a_mock.get_table_width = Mock(return_value=111)
+        a_mock.get_height = Mock(return_value=251)
+        a_mock.get_width = Mock(return_value=111)
         a_mock.get_cube_radius = Mock(return_value=4)
         a_mock.get_robot_radius = Mock(return_value=11)
         a_mock.get_atlas_zone_position = Mock(return_value=Point(95, 20))
+
         mock.return_value = a_mock
 
-    def adjustor(self):
+    @patch('Robot.path_finding.point_adjustor.config.Config')
+    def _adjustor(self, ConfigMock):
+        self._setup_config_mock(ConfigMock)
         point = PointAdjustor().find_target_position(self._cube.get_localization().position,
                                                      self._robot.get_localization().position)
         self.assertEqual(self._point_test, point)
 
-    def test_when_cube_is_near_west_wall(self, ConfigMock):
-        self._setup_config_mock(ConfigMock)
+    def test_when_cube_is_near_west_wall(self):
         self._point_test = Point(18, 50)
         self._cube.set_localization_position(Point(3, 50))
-        self.adjustor()
+        self._adjustor()
 
-    def test_when_cube_is_near_east_wall(self, ConfigMock):
-        self._setup_config_mock(ConfigMock)
+    def test_when_cube_is_near_east_wall(self):
         self._point_test = Point(95, 30)
         self._cube.set_localization_position(Point(110, 30))
-        self.adjustor()
+        self._adjustor()
 
-    def test_when_cube_is_near_north_wall(self, ConfigMock):
-        self._setup_config_mock(ConfigMock)
+    def test_when_cube_is_near_north_wall(self):
         self._point_test = Point(50, 236)
         self._cube.set_localization_position(Point(50, 251))
-        self.adjustor()
+        self._adjustor()
 
-    def test_when_cube_is_near_south_wall(self, ConfigMock):
-        self._setup_config_mock(ConfigMock)
+    def test_when_cube_is_near_south_wall(self):
         self._point_test = Point(80, 23)
         self._cube.set_localization_position(Point(80, 8))
-        self.adjustor()
+        self._adjustor()
 
-    def test_test_when_cube_is_top_right_of_robot(self, ConfigMock):
-        self._setup_config_mock(ConfigMock)
+    def test_when_cube_is_top_right_of_robot(self):
         self._point_test = Point(55, 70)
         self._cube.set_localization_position(Point(70, 70))
-        self.adjustor()
+        self._adjustor()
 
-    def test_test_when_cube_is_top_left_of_robot(self, ConfigMock):
-        self._setup_config_mock(ConfigMock)
+    def test_when_cube_is_top_left_of_robot(self):
         self._point_test = Point(33, 55)
         self._cube.set_localization_position(Point(33, 70))
-        self.adjustor()
+        self._adjustor()
 
-    def test_test_when_cube_is_bottum_left_of_robot(self, ConfigMock):
-        self._setup_config_mock(ConfigMock)
+    def test_when_cube_is_bottum_left_of_robot(self):
         self._point_test = Point(21, 30)
         self._cube.set_localization_position(Point(21, 15))
-        self.adjustor()
+        self._adjustor()
 
-    def test_test_when_cube_is_bottum_right_of_robot(self, ConfigMock):
-        self._setup_config_mock(ConfigMock)
+    def test_when_cube_is_bottum_right_of_robot(self):
         self._point_test = Point(55, 15)
         self._cube.set_localization_position(Point(70, 15))
-        self.adjustor()
+        self._adjustor()
 
-    def test_when_cube_is_near_wall_and_robot(self, ConfigMock):
-        self._setup_config_mock(ConfigMock)
+    def test_when_cube_is_near_wall_and_robot(self):
         self._point_test = Point(35, 60)
         self._cube.set_localization_position(Point(20, 60))
-        self.adjustor()
+        self._adjustor()
