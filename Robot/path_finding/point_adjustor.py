4b85c00e03e78b8bd8137032c5421c1ad1fdd1ae
Jean-Samuel <bedardjs97@hotmail.com>
2015-02-24 19:11:47 -0500

    now detects config mock (still not working)

--- Robot/path_finding/point_adjustor.py
+++ Robot/path_finding/point_adjustor.py
@@ -1,10 +1,11 @@
 from collections.__main__ import Point
-from Robot.path_finding.grid import SquareGrid
+
+from Robot.configuration import config
 
 
 class PointAdjustor():
+
     def __init__(self):
-        self._grid = SquareGrid()
         self._target_point = Point(0, 0)
         self._cube_center = Point(0, 0)
         self._robot_position = Point(0, 0)
@@ -15,32 +16,33 @@ class PointAdjustor():
                  call adjut_target_point to find the target position
                  closest to the robot.
     '''
+
     def check_if_cube_too_close_to_wall(self):
-        if (self._cube_center.x < self._grid.get_robot_radius()):
+        if (self._cube_center.x < config.Config().get_robot_radius()):
             self._target_point = Point(self._target_point.x +
-                                       self._grid.get_cube_radius() +
-                                       self._grid.get_robot_radius(),
+                                       config.Config().get_cube_radius() +
+                                       config.Config().get_robot_radius(),
                                        self._target_point.y)
 
-        elif (self._cube_center.x > (self._grid.get_width() -
-                                     self._grid.get_robot_radius())):
+        elif (self._cube_center.x > (config.Config().get_width() -
+                                     config.Config().get_robot_radius())):
             self._target_point = Point(self._target_point.x -
-                                       self._grid.get_cube_radius() -
-                                       self._grid.get_robot_radius(),
+                                       config.Config().get_cube_radius() -
+                                       config.Config().get_robot_radius(),
                                        self._target_point.y)
 
-        elif (self._cube_center.y > (self._grid.get_height() -
-                                     self._grid.get_robot_radius())):
+        elif (self._cube_center.y > (config.Config().get_height() -
+                                     config.Config().get_robot_radius())):
             self._target_point = Point(self._target_point.x,
                                        self._target_point.y -
-                                       self._grid.get_cube_radius() -
-                                       self._grid.get_robot_radius())
+                                       config.Config().get_cube_radius() -
+                                       config.Config().get_robot_radius())
 
-        elif (self._cube_center.y < self._grid.get_robot_radius()):
+        elif (self._cube_center.y < config.Config().get_robot_radius()):
             self._target_point = Point(self._target_point.x,
                                        self._target_point.y +
-                                       self._grid.get_cube_radius() +
-                                       self._grid.get_robot_radius())
+                                       config.Config().get_cube_radius() +
+                                       config.Config().get_robot_radius())
 
         else:
             self.adjust_target_point()
@@ -48,31 +50,31 @@ class PointAdjustor():
     def adjust_target_point(self):
         if (self._cube_center.x <= self._robot_position.x):
             if (self._cube_center.y <= (self._robot_position.y -
-                                        self._grid.get_robot_radius() -
-                                        self._grid.get_cube_radius())):
+                                        config.Config().get_robot_radius() -
+                                        config.Config().get_cube_radius())):
                 self._target_point = Point(self._target_point.x,
                                            self._target_point.y +
-                                           self._grid.get_cube_radius() +
-                                           self._grid.get_robot_radius())
+                                           config.Config().get_cube_radius() +
+                                           config.Config().get_robot_radius())
 
             elif (self._cube_center.y >= (self._robot_position.y +
-                                          self._grid.get_robot_radius() +
-                                          self._grid.get_cube_radius())):
+                                          config.Config().get_robot_radius() +
+                                          config.Config().get_cube_radius())):
                 self._target_point = Point(self._target_point.x,
                                            self._target_point.y -
-                                           self._grid.get_cube_radius() -
-                                           self._grid.get_robot_radius())
+                                           config.Config().get_cube_radius() -
+                                           config.Config().get_robot_radius())
 
             else:
                 self._target_point = (self._target_point.x +
-                                      self._grid.get_cube_radius() +
-                                      self._grid.get_robot_radius(),
+                                      config.Config().get_cube_radius() +
+                                      config.Config().get_robot_radius(),
                                       self._target_point.y)
 
         else:
             self._target_point = Point(self._target_point.x -
-                                       self._grid.get_cube_radius() -
-                                       self._grid.get_robot_radius(),
+                                       config.Config().get_cube_radius() -
+                                       config.Config().get_robot_radius(),
                                        self._target_point.y)
 
     def find_target_position(self, cube_position, robot_position):
--- tests/test_path_finding/test_PointAdjustor.py
+++ tests/test_path_finding/test_PointAdjustor.py
@@ -1,16 +1,18 @@
 from collections.__main__ import Point
+from defer import return_value
+from unittest.mock import patch, MagicMock, Mock
 import unittest
 
 from Robot.controller.robot import Robot
 from Robot.game_cycle.objects.color import Color
 from Robot.game_cycle.objects.cube import Cube
 from Robot.locators.localization import Localization
-from Robot.path_finding.grid import SquareGrid
 from Robot.path_finding.point_adjustor import PointAdjustor
-from unittest.mock import patch, MagicMock
 
 
+@patch('Robot.path_finding.point_adjustor.config.Config')
 class GameTest(unittest.TestCase):
+
     @classmethod
     def setUpClass(cls):
         cls._cube = Cube(Color.RED, 0, False, Localization(Point(0, 0), 0))
@@ -20,54 +22,71 @@ class GameTest(unittest.TestCase):
         self._robot.set_localization_position(Point(50, 50))
         self._point_test = Point(0, 0)
 
-    @patch('Robot.path_finding')
-    def adjustor(self, GridMock):
-        self._grid = GridMock
+    def _setup_config_mock(self, mock):
+
+        a_mock = MagicMock()
+        a_mock.get_table_height = Mock(return_value=251)
+        a_mock.get_table_width = Mock(return_value=111)
+        a_mock.get_cube_radius = Mock(return_value=4)
+        a_mock.get_robot_radius = Mock(return_value=11)
+        a_mock.get_atlas_zone_position = Mock(return_value=Point(95, 20))
+        mock.return_value = a_mock
+
+    def adjustor(self):
         point = PointAdjustor().find_target_position(self._cube.get_localization().position,
                                                      self._robot.get_localization().position)
         self.assertEqual(self._point_test, point)
 
-    def test_when_cube_is_near_west_wall(self):
+    def test_when_cube_is_near_west_wall(self, ConfigMock):
+        self._setup_config_mock(ConfigMock)
         self._point_test = Point(18, 50)
         self._cube.set_localization_position(Point(3, 50))
         self.adjustor()
 
-    def test_when_cube_is_near_east_wall(self):
+    def test_when_cube_is_near_east_wall(self, ConfigMock):
+        self._setup_config_mock(ConfigMock)
         self._point_test = Point(95, 30)
         self._cube.set_localization_position(Point(110, 30))
         self.adjustor()
 
-    def test_when_cube_is_near_north_wall(self):
+    def test_when_cube_is_near_north_wall(self, ConfigMock):
+        self._setup_config_mock(ConfigMock)
         self._point_test = Point(50, 236)
         self._cube.set_localization_position(Point(50, 251))
         self.adjustor()
 
-    def test_when_cube_is_near_south_wall(self):
+    def test_when_cube_is_near_south_wall(self, ConfigMock):
+        self._setup_config_mock(ConfigMock)
         self._point_test = Point(80, 23)
         self._cube.set_localization_position(Point(80, 8))
         self.adjustor()
 
-    def test_test_when_cube_is_top_right_of_robot(self):
+    def test_test_when_cube_is_top_right_of_robot(self, ConfigMock):
+        self._setup_config_mock(ConfigMock)
         self._point_test = Point(55, 70)
         self._cube.set_localization_position(Point(70, 70))
         self.adjustor()
 
-    def test_test_when_cube_is_top_left_of_robot(self):
+    def test_test_when_cube_is_top_left_of_robot(self, ConfigMock):
+        self._setup_config_mock(ConfigMock)
         self._point_test = Point(33, 55)
         self._cube.set_localization_position(Point(33, 70))
         self.adjustor()
 
-    def test_test_when_cube_is_bottum_left_of_robot(self):
+    def test_test_when_cube_is_bottum_left_of_robot(self, ConfigMock):
+        self._setup_config_mock(ConfigMock)
         self._point_test = Point(21, 30)
         self._cube.set_localization_position(Point(21, 15))
         self.adjustor()
 
-    def test_test_when_cube_is_bottum_right_of_robot(self):
+    def test_test_when_cube_is_bottum_right_of_robot(self, ConfigMock):
+        self._setup_config_mock(ConfigMock)
         self._point_test = Point(55, 15)
         self._cube.set_localization_position(Point(70, 15))
         self.adjustor()
 
-    def test_when_cube_is_near_wall_and_robot(self):
+    def test_when_cube_is_near_wall_and_robot(self, ConfigMock):
+        self._setup_config_mock(ConfigMock)
         self._point_test = Point(35, 60)
         self._cube.set_localization_position(Point(20, 60))
         self.adjustor()
