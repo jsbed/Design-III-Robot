1b9e2898677419e2dd21e0a5056dc5f478ccd3cc
dms1lva <dario@live.ca>
2015-03-01 20:32:57 -0500

    removed self from tests

--- tests/test_question_analysis/test_question_matchers.py
+++ tests/test_question_analysis/test_question_matchers.py
@@ -4,12 +4,12 @@ from Robot.question_analysis.matchers import GrowthRateBetween
 from Robot.question_analysis.matchers.question_matchers import UrbanAreasAre
 
 
-def test_urban_areas(self):
+def test_urban_areas():
     question = 'The major urban areas of this country are Santiago, Valparaiso and Concepcion.'
     urban_areas_are = UrbanAreasAre()
     assert_true(urban_areas_are.find_info(question))
 
-def test_growth_rate_between(self):
+def test_growth_rate_between():
     question = 'My population growth rate is between 15% and 25%'
     growth_rate_between = GrowthRateBetween()
     assert_true(growth_rate_between.find_info(question))
