a83299d4976dbb4d21f7a6684a0dead4c5f0b0c1
Jean-Samuel <devnull@localhost>
2015-03-06 15:44:00 -0500

    new vision test

--- Experiments/Vision/kinect_test.py
+++ Experiments/Vision/kinect_test.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import sys, time, cv2, os
+import sys
+import time
+import cv2
+import os
 import numpy as np
 
 SS_DIRECTORY = "screenshots"
@@ -19,32 +22,34 @@ flags, img = captObj.read()
 time.sleep(1)
 
 while True:
-	# On recupere une nouvelle image
-	captObj.grab()
-	
-	# On va chercher les infos
-	flags_i, img_i = captObj.retrieve(None, cv2.CAP_OPENNI_BGR_IMAGE)
-	flags_p, img_p = captObj.retrieve(None, cv2.CAP_OPENNI_DEPTH_MAP)
-	flags_g, img_g = captObj.retrieve(None, cv2.CAP_OPENNI_GRAY_IMAGE)
+    # On recupere une nouvelle image
+    captObj.grab()
 
-	# Pas d'image, peut se produire si on boucle trop vite
-	if not flags_i or not flags_p or not flags_g:
-		continue
-	
-	# Affichage
-	cv2.imshow('Affichage RGB', img_i)
-	cv2.imshow('Affichage profondeur', img_p)
+    # On va chercher les infos
+    flags_i, img_i = captObj.retrieve(None, cv2.CAP_OPENNI_BGR_IMAGE)
+    flags_p, img_p = captObj.retrieve(None, cv2.CAP_OPENNI_POINT_CLOUD_MAP)
+    flags_g, img_g = captObj.retrieve(None, cv2.CAP_OPENNI_GRAY_IMAGE)
 
-	#cv2.imshow('Affichage Gris', img_g)
+    # Pas d'image, peut se produire si on boucle trop vite
+    if not flags_i or not flags_p or not flags_g:
+        continue
 
-	cc = cv2.waitKey(10) # Necessaire pour l'affichage effectif des images
-	if cc == 27: # Touche Echap quitte
-		break
+    # Affichage
+    cv2.imshow('Affichage RGB', img_i)
+    cv2.imshow('Affichage profondeur', img_p)
 
-	if cc == 10: # Touche Enter 
-		np.save(os.path.join(SS_DIRECTORY, "ss_{}_p".format(str(SS_COUNT))), img_p)
-		cv2.imwrite(os.path.join(SS_DIRECTORY, "ss_{}.jpg".format(str(SS_COUNT))), img_i)
-		print("ss_" + str(SS_COUNT) + " taken")
-		SS_COUNT += 1
-    
-	#if cc != -1 : print(cc)
+    #cv2.imshow('Affichage Gris', img_g)
+
+    cc = cv2.waitKey(10)  # Necessaire pour l'affichage effectif des images
+    if cc == 27:  # Touche Echap quitte
+        break
+
+    if cc == 10:  # Touche Enter
+        np.save(
+            os.path.join(SS_DIRECTORY, "ss_{}_p".format(str(SS_COUNT))), img_p)
+        cv2.imwrite(
+            os.path.join(SS_DIRECTORY, "ss_{}.jpg".format(str(SS_COUNT))), img_i)
+        print("ss_" + str(SS_COUNT) + " taken")
+        SS_COUNT += 1
+
+    #if cc != -1 : print(cc)
--- Experiments/Vision/perspective/pixel_and_depth_finder_kinect.py
+++ Experiments/Vision/perspective/pixel_and_depth_finder_kinect.py
@@ -5,12 +5,18 @@ from math import cos, radians, sin
 import numpy
 import time
 
+from Robot.configuration.config import Config
+from Robot.game_cycle.objects.color import Color
+from Robot.locators.location_computer import robot_location_computer
+from Robot.locators.robot_corners.robot_corner import RobotCorner
+from Robot.path_finding.point import Point
+
 
 captObj = cv2.VideoCapture(cv2.CAP_OPENNI)
 flags, img = captObj.read()
 time.sleep(1)
 
-angleY = 21.5
+angleY = 22
 
 rotateY = numpy.array([[cos(radians(angleY)), 0, sin(radians(angleY))],
                        [0, 1, 0],
@@ -21,31 +27,37 @@ revert_x = numpy.array([[-1, 0, 0],
                         [0, 0, 1]])
 
 
-virtual_coord = [(299, 407), (277, 563), (274, 313), (292, 134)]
+virtual_coord = [(285, 395), (257, 550), (259, 301), (282, 123)]
 
 real_data = numpy.float32(
-    [[0.31, 0.23], [0.08, 1.50], [1.03, 1.50], [0.80, 0.23]])
+    [[0.305, 0.225], [0.08, 1.50], [1.03, 1.50], [0.805, 0.225]])
 
-translation_mat = numpy.array([0.12, 0.13, -0.54])
+translation_mat = numpy.array([0.125, 0.13, -0.54])
 
 perspective_transform = numpy.load("perspective_array.npy")
+
+last_new_point = 0
+close_corner = 0
+far_corner = 0
+last_x = 0
+
+Config("../../../Robot/config.ini").load_config()
 
 
 def MouseEventCallback(event, x, y, flag, data):
     if event == cv2.EVENT_LBUTTONDOWN:
-        # print(x)
-        # print(y)
         print(y, x)
-        transformed = transform((y, x))
+        last_x = x
+        #transformed = transform((y, x))
 
         print("real", img_p[y, x])
-        print("transformed", transformed)
+        #print("transformed", transformed)
+        new_point = full_transform((y, x))
+        # last_new_point = cv2.perspectiveTransform(
+        #    numpy.float32([[[transformed[0], transformed[2]]]]),
+        #    perspective_transform)
 
-        newPoint = cv2.perspectiveTransform(
-            numpy.float32([[[transformed[0], transformed[2]]]]),
-            perspective_transform)
-
-        print("final", newPoint)
+        print("final", new_point)
 
 
 # Create a black image, a window
@@ -58,8 +70,20 @@ cv2.setMouseCallback("image", MouseEventCallback)
 def transform(point):
     revert = numpy.dot(revert_x, img_p[point])
     rotation_transform = numpy.dot(rotateY, revert)
-    return rotation_transform + translation_mat
+    after_transform = rotation_transform + translation_mat
 
+    print("transformed", after_transform)
+
+    return after_transform
+
+
+def full_transform(point):
+    post_transform = transform(point)
+    last_new_point = cv2.perspectiveTransform(
+        numpy.float32([[[post_transform[0], post_transform[2]]]]),
+        perspective_transform)
+
+    return numpy.squeeze(last_new_point)
 
 while True:
     # On recupere une nouvelle image
@@ -81,7 +105,27 @@ while True:
     if cc == 27:  # Touche Echap quitte
         break
 
-    elif cc == 10:  # Touche Enter compute GetPerspective
+    elif cc == 49:  # Touche '1' pour close corner
+        close_corner = numpy.squeeze(last_new_point)
+        print(last_x)
+        print(close_corner)
+
+    elif cc == 50:  # Touche '2' pour far corner
+        far_corner = numpy.squeeze(last_new_point)
+
+    elif cc == 32:
+        #  [ 0.53602403  1.16192186  ] close
+        #  [ 0.38169086  1.31478822 ] far
+        close_robot_corner = RobotCorner(Point(100 * 0.53602403, 100 * 1.19192186),
+                                         Color.NONE)
+        far_robot_corner = RobotCorner(Point(100 * 0.38169086, 100 * 1.34478822),
+                                       Color.NONE)
+
+        localization = robot_location_computer.compute(close_robot_corner,
+                                                       far_robot_corner)
+        print(localization.position, localization.orientation)
+
+    elif cc == 10:  # Touche Enter compute GetPerspect   ive
         first_point = transform(virtual_coord[0])
         second_point = transform(virtual_coord[1])
         third_point = transform(virtual_coord[2])
