f86b1c48e0eaa863d4ca727cfad34798edf311e9
Jean-Samuel <bedardjs97@hotmail.com>
2015-03-21 19:53:52 -0400

    a location can now be unknown

--- kinect_test.py
+++ kinect_test.py
@@ -15,10 +15,10 @@ while(1):
     cv2.imshow("img", bgr)
     #cv2.imshow("cloud", depth)
 
-    try:
-        localization = robot_locator.localize()
-    except Exception as e:
-        print(str(e))
+    localization = robot_locator.localize()
+
+    if (localization.unknown):
+        print("robot location unknown")
     else:
         print(localization.position, localization.orientation)
 
