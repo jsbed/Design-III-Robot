048a09a95b1c3967031441388f84ceb35d657dfb
Jean-Samuel <devnull@localhost>
2015-03-21 12:37:18 -0400

    now also send an adjustment value for the gripper

--- Robot/managers/gripper_manager.py
+++ Robot/managers/gripper_manager.py
@@ -1,5 +1,7 @@
 from Robot.configuration import config
 
+CERVO_POSITION_INDEX = 3
+
 
 class GripperManager():
 
@@ -7,21 +9,35 @@ class GripperManager():
         self._serial_port = serial_port
 
     def take_cube(self):
-        self._serial_port.send_array_of_ints(
-            config.Config().get_take_cube_gripper_values())
+        array_of_ints = config.Config().get_take_cube_gripper_values()
+
+        self._serial_port.send_array_of_ints(array_of_ints)
+        self._send_adjustment(array_of_ints)
 
     def release_cube(self):
-        self._serial_port.send_array_of_ints(
-            config.Config().get_release_cube_gripper_values())
+        array_of_ints = config.Config().get_release_cube_gripper_values()
+
+        self._serial_port.send_array_of_ints(array_of_ints)
+        self._send_adjustment(array_of_ints)
 
     def lift_gripper(self):
-        self._serial_port.send_array_of_ints(
-            config.Config().get_lift_gripper_values())
+        array_of_ints = config.Config().get_lift_gripper_values()
+
+        self._serial_port.send_array_of_ints(array_of_ints)
+        self._send_adjustment(array_of_ints)
 
     def lower_gripper(self):
-        self._serial_port.send_array_of_ints(
-            config.Config().get_lower_gripper_values())
+        array_of_ints = config.Config().get_lower_gripper_values()
+
+        self._serial_port.send_array_of_ints(array_of_ints)
+        self._send_adjustment(array_of_ints)
 
     def widest_gripper(self):
-        self._serial_port.send_array_of_ints(
-            config.Config().get_widest_gripper_values())
+        array_of_ints = config.Config().get_widest_gripper_values()
+
+        self._serial_port.send_array_of_ints(array_of_ints)
+        self._send_adjustment(array_of_ints)
+
+    def _send_adjustment(self, array):
+        array[CERVO_POSITION_INDEX] -= 2
+        self._serial_port.send_array_of_ints(array)
--- tests/test_managers/test_gripper_manager.py
+++ tests/test_managers/test_gripper_manager.py
@@ -5,10 +5,15 @@ import unittest
 from Robot.managers.gripper_manager import GripperManager
 
 TAKE_CUBE_VALUES = [0, 1, 2, 3]
+TAKE_CUBE_ADJUSTMENT_VALUES = [0, 1, 2, 1]
 RELEASE_CUBE_VALUES = [1, 2, 3, 4]
+RELEASE_CUBE_ADJUSTMENT_VALUES = [1, 2, 3, 2]
 LIFT_GRIPPER_VALUES = [2, 3, 4, 5]
+LIFT_GRIPPER_ADJUSTMENT_VALUES = [2, 3, 4, 3]
 LOWER_GRIPPER_VALUES = [3, 4, 5, 6]
+LOWER_GRIPPER_ADJUSTMENT_VALUES = [3, 4, 5, 4]
 WIDEST_GRIPPER_VALUES = [4, 5, 6, 7]
+WIDEST_GRIPPER_ADJUSTMENT_VALUES = [4, 5, 6, 5]
 
 
 @patch("Robot.configuration.config.Config")
@@ -25,7 +30,7 @@ class GripperManagerTest(unittest.TestCase):
         serial_mock.send_array_of_ints = Mock()
         return serial_mock
 
-    def test_when_take_cube_should_call_serial_with_config_take_cube_values(self, config_mock):
+    def test_when_take_cube_should_call_serial_with_config_take_cube_values_with_its_adjustment(self, config_mock):
         custom_config_mock = MagicMock()
         custom_config_mock.get_take_cube_gripper_values = Mock(
             return_value=TAKE_CUBE_VALUES)
@@ -35,8 +40,10 @@ class GripperManagerTest(unittest.TestCase):
 
         self.serial_mock.send_array_of_ints.assert_called_with(
             TAKE_CUBE_VALUES)
+        self.serial_mock.send_array_of_ints.assert_called_with(
+            TAKE_CUBE_ADJUSTMENT_VALUES)
 
-    def test_when_release_cube_should_call_serial_with_config_release_cube_values(self, config_mock):
+    def test_when_release_cube_should_call_serial_with_config_release_cube_values_with_its_adjustment(self, config_mock):
         custom_config_mock = MagicMock()
         custom_config_mock.get_release_cube_gripper_values = Mock(
             return_value=RELEASE_CUBE_VALUES)
@@ -46,8 +53,10 @@ class GripperManagerTest(unittest.TestCase):
 
         self.serial_mock.send_array_of_ints.assert_called_with(
             RELEASE_CUBE_VALUES)
+        self.serial_mock.send_array_of_ints.assert_called_with(
+            RELEASE_CUBE_ADJUSTMENT_VALUES)
 
-    def test_when_lift_gripper_should_call_serial_with_config_lift_gripper_values(self, config_mock):
+    def test_when_lift_gripper_should_call_serial_with_config_lift_gripper_values_with_its_adjustment(self, config_mock):
         custom_config_mock = MagicMock()
         custom_config_mock.get_lift_gripper_values = Mock(
             return_value=LIFT_GRIPPER_VALUES)
@@ -57,8 +66,10 @@ class GripperManagerTest(unittest.TestCase):
 
         self.serial_mock.send_array_of_ints.assert_called_with(
             LIFT_GRIPPER_VALUES)
+        self.serial_mock.send_array_of_ints.assert_called_with(
+            LIFT_GRIPPER_ADJUSTMENT_VALUES)
 
-    def test_when_lower_gripper_should_call_serial_with_config_lower_gripper_values(self, config_mock):
+    def test_when_lower_gripper_should_call_serial_with_config_lower_gripper_values_with_its_adjustment(self, config_mock):
         custom_config_mock = MagicMock()
         custom_config_mock.get_lower_gripper_values = Mock(
             return_value=LOWER_GRIPPER_VALUES)
@@ -68,8 +79,10 @@ class GripperManagerTest(unittest.TestCase):
 
         self.serial_mock.send_array_of_ints.assert_called_with(
             LOWER_GRIPPER_VALUES)
+        self.serial_mock.send_array_of_ints.assert_called_with(
+            LOWER_GRIPPER_ADJUSTMENT_VALUES)
 
-    def test_when_widest_gripper_should_call_serial_with_config_widest_gripper_values(self, config_mock):
+    def test_when_widest_gripper_should_call_serial_with_config_widest_gripper_values_with_its_adjustment(self, config_mock):
         custom_config_mock = MagicMock()
         custom_config_mock.get_widest_gripper_values = Mock(
             return_value=WIDEST_GRIPPER_VALUES)
@@ -79,3 +92,5 @@ class GripperManagerTest(unittest.TestCase):
 
         self.serial_mock.send_array_of_ints.assert_called_with(
             WIDEST_GRIPPER_VALUES)
+        self.serial_mock.send_array_of_ints.assert_called_with(
+            WIDEST_GRIPPER_ADJUSTMENT_VALUES)
