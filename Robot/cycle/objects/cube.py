2420d50438100026f9ee788039638eb299374ec2
Jean-Samuel <bedardjs97@hotmail.com>
2015-03-22 14:12:06 -0400

    fixed unknown localization instanciation

--- Robot/cycle/objects/cube.py
+++ Robot/cycle/objects/cube.py
@@ -1,5 +1,4 @@
 from Robot.locators.localization import Localization
-from Robot.path_finding.point import Point
 
 
 class Cube:
@@ -7,7 +6,7 @@ class Cube:
     def __init__(self, color, target_zone_position, index=0):
         self._color = color
         self._target_zone_position = target_zone_position
-        self._localization = Localization(Point(-1, -1), 0)
+        self._localization = Localization(None, None, unknown=True)
         self._index = index
 
     def set_localization_position(self, value):
