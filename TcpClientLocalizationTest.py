5be29232906571ac17685fb21eb9b7ae1710b3fa
Jean-Samuel <bedardjs97@hotmail.com>
2015-03-26 15:15:49 -0400

    tested localization request with BaseStationClient

--- TcpClientLocalizationTest.py
+++ TcpClientLocalizationTest.py
@@ -1,28 +1,14 @@
-import zmq
-
-from Robot.communication.localization import localization_response, localization_request
+from Robot.communication.base_station_client import BaseStationClient
 from Robot.configuration.config import Config
 
 
 Config().load_config()
 
 
-# Create a TCP/IP socket
-context = zmq.Context()
-socket = context.socket(zmq.DEALER)  # @UndefinedVariable
-url = "tcp://{}:{}".format(
-    Config().get_base_station_communication_ip(),
-    Config().get_base_station_communication_port())
-socket.connect(url)
-print('connecting to ' + url)
-
-
-socket.send(
-    bytes(localization_request.create_robot_localization_request(), "utf-8"))
+BaseStationClient().connect_socket()
 print("waiting")
-a = socket.recv().decode("utf-8")
+loc = BaseStationClient().request_robot_localization()
 print("received")
-loc = localization_response.create_localization_from_localization_response(a)
 print(loc.position)
 print(loc.orientation)
 print(loc.unknown)
