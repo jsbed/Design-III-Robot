b43c8ea7e069482b77abe040b87edbb6803b1234
jsbed <bedardjs97@hotmail.com>
2015-03-30 16:55:50 -0400

    added some more code

--- BaseStation/communication/tcp_server.py
+++ BaseStation/communication/tcp_server.py
@@ -1,13 +1,11 @@
-from PySide.QtCore import QThread
 import json
+
+from PySide.QtCore import QThread
 import zmq
 
 from BaseStation.ui.utilities.Signal import Signal
 from Robot.communication.localization.localization_dto import create_localization_dto
-from Robot.communication.localization.localization_request import ROBOT_LOCALIZATION_REQUEST, CUBE_LOCALIZATION_REQUEST
 from Robot.configuration.config import Config
-from Robot.cycle.objects.color import Color
-from Robot.locators import robot_locator, cube_locator
 
 
 QUESTION_OK_SIGNAL = "question ok signal"
@@ -45,40 +43,13 @@ class TcpServer(QThread):
         while True:
             try:
                 data = self._socket.recv().decode("utf-8")
-                self._interprete_received_data(data)
+                self.signal.custom_signal.emit(data)
             except Exception as e:
                 self._send_message("Base Station Server error: " + str(e))
-
-    def _interprete_received_data(self, received_data):
-        if "request" in received_data:
-            self._handle_robot_request(received_data)
-        else:
-            self.signal.custom_signal.emit(received_data)
 
     def _send_message(self, message):
         self.signal.custom_signal.emit(json.dumps({"message": message}))
 
-    def _handle_robot_request(self, received_data):
-        received_data = json.loads(received_data)
-
-        if received_data["request"] == ROBOT_LOCALIZATION_REQUEST:
-            self._send_robot_localization_response()
-        elif received_data["request"] == CUBE_LOCALIZATION_REQUEST:
-            self._send_cube_localization_response(
-                Color(received_data["color"]))
-
-    def _send_robot_localization_response(self):
-        robot_localization = robot_locator.localize()
-
-        while robot_localization.unknown:
-            robot_localization = robot_locator.localize()
-
-        self._send_localization(robot_localization)
-
-    def _send_cube_localization_response(self, color):
-        cube_localization = cube_locator.localize_with_kinect(color)
-        self._send_localization(cube_localization)
-
-    def _send_localization(self, localization):
+    def send_localization_response(self, localization):
         localization_dto = create_localization_dto(localization)
         self._socket.send(bytes(localization_dto, "utf-8"))
--- BaseStation/workers/robot_locator_worker.py
+++ BaseStation/workers/robot_locator_worker.py
@@ -1,28 +1,40 @@
-from PySide.QtCore import QThread
 import time
+
+from PySide.QtCore import QThread
 
 from BaseStation.ui.utilities.Signal import Signal
 from Robot.communication.localization.localization_dto import create_localization_dto
 from Robot.locators import robot_locator
+from Robot.locators.robot_localization_filter import RobotLocalizationFilter, ROBOT_LOCALIZATION_UPDATED
+from Robot.utilities.observer import Observer
 
 
-class RobotLocatorWorker(QThread):
+class RobotLocatorWorker(Observer, QThread):
 
     def __init__(self):
         QThread.__init__(self)
         self.signal = Signal()
+        self._robot_localization_filter = RobotLocalizationFilter()
         self._running = False
+
+        self._robot_localization_filter.attach(
+            ROBOT_LOCALIZATION_UPDATED, self)
 
     def run(self):
         self._running = True
 
         while self._running:
             localization = robot_locator.localize()
+            self._robot_localization_filter.update_localization(localization)
 
-            if not localization.unknown:
-                localization_dto = create_localization_dto(localization)
-                self.signal.custom_signal.emit(localization_dto)
-                time.sleep(0.5)
+    def observer_update(self, event, value):
+        if (event == ROBOT_LOCALIZATION_UPDATED):
+            localization_dto = create_localization_dto(value)
+            self.signal.custom_signal.emit(localization_dto)
+            # time.sleep(0.5)
 
     def stop(self):
         self._running = False
+
+    def get_localization(self):
+        self._robot_localization_filter.get_localization()
