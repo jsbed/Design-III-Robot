8c376d179b09ef64a12738e320461113cb5c7c0e
Jean-Samuel <bedardjs97@hotmail.com>
2015-04-04 17:46:54 -0400

    cleaned cube locator for release

--- Robot/locators/cube_locator.py
+++ Robot/locators/cube_locator.py
@@ -1,9 +1,3 @@
-import cv2
-import numpy
-import time
-
-from Robot.configuration.config import Config
-from Robot.cycle.objects.color import Color
 from Robot.locators.contour import contours_finder
 from Robot.locators.extractors.cube import cube_extractor_factory
 from Robot.locators.localization import Localization
@@ -18,17 +12,9 @@ def localize_with_camera(cube_color):
     extracted_cube = extractor.extract_cube(original_image)
 
     corners = contours_finder.find_cube_corners_contours(extracted_cube)
+
     # TODO : EXTRACT CUBE LOCALIZATION
     cube_location_computer.compute_for_camera(corners)
-    #cv2.imwrite("test.jpg", extracted_cube)
-
-    for a in corners:
-        cv2.circle(extracted_cube, (a[0], a[1]), 3, (0, 0, 255))
-
-    cv2.imshow("cube", extracted_cube)
-    #cv2.imwrite("with_contour.jpg", extracted_cube)
-
-# Does not work with a White or Black cube
 
 
 def localize_with_kinect(cube_color):
@@ -38,23 +24,7 @@ def localize_with_kinect(cube_color):
             cube_color)
         extracted_cube = cube_extractor.extract_cube(img_bgr)
 
-        return cube_location_computer.compute_for_kinect(extracted_cube, img_cloud)
+        return cube_location_computer.compute_for_kinect(extracted_cube,
+                                                         img_cloud)
     except:
         return Localization(None, None, unknown=True)
-
-
-Config("../../config.ini").load_config()
-
-Camera().start()
-
-time.sleep(1.5)
-
-while 1:
-    cc = cv2.waitKey(2)
-
-    if cc == 27:
-        break
-    else:
-        localize_with_camera(Color.YELLOW)
-
-Camera().stop()
