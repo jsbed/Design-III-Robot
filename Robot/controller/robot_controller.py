49be32daeb00f4ab192c5b851a750114a1f818b8
design3-box <dario@live.ca>
2015-04-13 21:01:26 -0400

    fixed robot behavior for localization

--- Robot/controller/robot_controller.py
+++ Robot/controller/robot_controller.py
@@ -108,7 +108,8 @@ class RobotController():
                                               localization_position)
 
         rotation = LOCALIZE_CUBE_ANGLE - self._robot_orientation
-        if (abs(rotation) > config.Config().get_orientation_uncertainty()):
+        if (abs(rotation) > config.Config().get_orientation_uncertainty() or
+                self._distance > 40):
             self._move_robot_towards_target_point(localization_position)
             self._append_rotations(
                 -PointAdjustor().find_robot_rotation(LOCALIZE_CUBE_ANGLE,
@@ -118,7 +119,7 @@ class RobotController():
             self._robot.append_instruction(
                 MoveForward(localization_position.y - self._robot_position.y))
             self._robot.append_instruction(
-                MoveRight(localization_position.x - self._robot_position.x))
+                MoveRight(self._robot_position.x - localization_position.x))
 
         self._robot.execute_instructions()
 
